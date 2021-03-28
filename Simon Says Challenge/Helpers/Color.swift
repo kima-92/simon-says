c7557e74164b9d0b04f29044dfe03a6b3284728b
Wilma <kima_92@fastmail.com>
2021-03-28 16:51:27 -0400

    Creating the Color enum

---
+++ Simon Says Challenge/Helpers/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Colors.swift
+//  Simon Says Challenge
+//
+//  Created by macbook on 3/28/21.
+//
+
+import Foundation
+
+enum Color: String {
+    case red = "red"
+    case blue = "blue"
+    case purple = "purple"
+    case yellow = "yellow"
+}
